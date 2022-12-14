b556b890a74af7ccc94851971a4437b694b39d08
Konark Gautam <rahulkonarks@gmail.com>
2022-12-14 21:57:59 +0400

    Construct a message with titles, descriptions and links. Send the email with a subject.

--- email_news.py
+++ email_news.py
@@ -1,9 +1,11 @@
 import requests as rq
+from send_email import send_email
 
+topic = "tesla"
 with open('/home/konarkguatam/API_KEY.txt', 'r') as file:
     api_key = file.read().strip('\n')
     url = f"https://newsapi.org/v2/everything?" \
-          f"q=tesla" \
+          f"q={topic}" \
           f"&from=2022-11-14" \
           f"&sortBy=publishedAt" \
           f"&apiKey={api_key}" \
@@ -14,3 +16,12 @@ content = request.json()
 
 body = ""
 
+for article in content['articles'][:20]:
+    if article['title'] is not None:
+        body = "Subject: Today's news" + '\n'\
+               + body + article['title'] + '\n'\
+               + article['description'] + '\n'\
+               + article['url'] + 2 * '\n'
+
+body = body.encode('utf-8')
+send_email(message=body)
